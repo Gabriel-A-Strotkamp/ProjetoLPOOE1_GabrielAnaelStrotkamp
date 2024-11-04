e2c1d0e7c580397ec2e905f0724621e265a556a6
Gabriel Anael Strotkamp <127111398+Gabriel-A-Strotkamp@users.noreply.github.com>
2024-11-04 11:31:00 -0300

    Add files via upload

---
+++ src/main/java/Testes/testes01.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Testes;
+
+/**
+ *
+ * @author 20221PF.CC0008
+ */
+import javax.persistence.Entity;
+
+@Entity
+public class testes01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
